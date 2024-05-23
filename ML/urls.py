0630b59ac350bcb40c3412335fb0edf585b502d0
le1ezera <lkm19980706@gmail.com>
2024-05-23 10:54:28 +0800

    add single topic

--- ML/urls.py
+++ ML/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- ML/views.py
+++ ML/views.py
@@ -11,3 +11,9 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'ML/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'ML/topic.html', context)
