c15a18af38842569816c91e421e7ba55b2cfa893
le1ezera <lkm19980706@gmail.com>
2024-05-23 09:02:14 +0800

    add topic.html

--- ML/views.py
+++ ML/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from ML.models import Topic
+
+
 # Create your views here.
 def index(request):
     return render(request, 'ML/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'ML/topics.html', context)
