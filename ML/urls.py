3efda3019d57dab2b1416a07d8e561496d8989fd
le1ezera <lkm19980706@gmail.com>
2024-05-25 09:17:28 +0800

    add new topic

--- ML/urls.py
+++ ML/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.base, name='base'),
+    path('index', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('topics/new_topic/', views.new_topic, name='new_topic'),
 ]
--- ML/views.py
+++ ML/views.py
@@ -1,11 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from ML.forms import TopicForm
 from ML.models import Topic
 
 
 # Create your views here.
 def index(request):
     return render(request, 'ML/index.html')
+
+def base(requeset):
+    return render(requeset, 'ML/base.html')
 
 def topics(request):
     topics = Topic.objects.order_by('date_added')
@@ -17,3 +23,14 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'ML/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('ML:topics'))
+    context = {'form': form}
+    return render(request, 'ML/new_topic.html', context)
