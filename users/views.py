8de897d304d1bd731cdb0a59de4188855388945e
le1ezera <lkm19980706@gmail.com>
2024-05-27 16:15:39 +0800

    add log out

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('ML/index'))
