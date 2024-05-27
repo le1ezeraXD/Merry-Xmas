cb13045a1a0b3e5c35df3d2f9df8157b41f9d126
le1ezera <lkm19980706@gmail.com>
2024-05-27 16:08:22 +0800

    add login.html

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 #path是Django 2的用法
 #from django.conf.urls import url 是1.1的用法
-from django.contrib.auth import login
-
+# from django.contrib.auth import login
+#同理，上面也是1.1的用法
+#下面才是2的用法
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
