d3cf014c0a3ae331cc0dc1f9f578439ba3b4338c
le1ezera <lkm19980706@gmail.com>
2024-05-28 08:58:13 +0800

    remove logout in views

--- users/views.py
+++ users/views.py
@@ -4,7 +4,3 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import logout
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('ML/index'))
