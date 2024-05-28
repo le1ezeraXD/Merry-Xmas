33cd2e25d6303205250bfdaf1b8595acf18287fb
le1ezera <lkm19980706@gmail.com>
2024-05-28 09:34:23 +0800

    add register

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,5 @@ from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='ML/index.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=form.cleaned_data['username'],
+                                              password=form.cleaned_data['<PASSWORD>'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('index'))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
