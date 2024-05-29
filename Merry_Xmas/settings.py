e83cd788285abe77bd9c598e2c207f750b7c760c
le1ezera <lkm19980706@gmail.com>
2024-05-29 10:31:40 +0800

    update static files settings

--- Merry_Xmas/settings.py
+++ Merry_Xmas/settings.py
@@ -137,14 +137,14 @@ BOOTSTRAP3 = {
 if os.getcwd() == '/app':
     import dj_database_url
     DATABASES = {
-        'default': dj_database_url.config(default='postgres://localhost/Merry_Xmas')
+        'default': dj_database_url.config(default='postgres://localhost')
     }
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     ALLOWED_HOSTS = ['*']
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = {
         os.path.join(BASE_DIR, 'static')
     }
