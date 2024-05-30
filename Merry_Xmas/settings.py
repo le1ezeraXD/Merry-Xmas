ef9720255a8bce42a0a27fbb8e24e0a67092afc0
le1ezera <lkm19980706@gmail.com>
2024-05-30 11:03:10 +0800

    add 'OPTIONS' in settings.py and add 'service'

--- Merry_Xmas/settings.py
+++ Merry_Xmas/settings.py
@@ -83,7 +83,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
-        "service": "django.db.backends.sqlite3",
+        'OPTIONS': {
+            'service': 'merryxmas',
+        },
     }
 }
 
